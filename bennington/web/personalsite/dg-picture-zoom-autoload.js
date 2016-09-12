1e5f470e1a4fcb260d372eac56ed05cecdc371eb
Brendon Walter <brendongwalter@gmail.com>
2016-09-11 19:56:03 -0700

    Reworked the entire site

---
+++ bennington/web/personalsite/dg-picture-zoom-autoload.js
@@ -0,0 +1,9 @@
+var dgPictureZoom = new DG.PictureZoom({
+  border : false
+});
+
+<link rel="stylesheet" href="/scripts/picture-zoom/css/dg-picture-zoom.css" type="text/css" />
+   <script src="/scripts/picture-zoom/js/external/mootools-1.2.4-core-yc.js"></script>
+   <script src="/scripts/picture-zoom/js/external/mootools-more.js"></script>
+   <script src="/scripts/picture-zoom/js/dg-picture-zoom.js"></script>
+   <script src="/scripts/picture-zoom/js/dg-picture-zoom-autoload.js"></script>
